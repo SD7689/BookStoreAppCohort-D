9d999ac829d247bd3a9c9f00ef411e2d02cbac5d
Anoop kumar <kanoop6401@gmail.com>
2020-05-07 11:24:00 +0530

    [Anoop] Add. Repository and add AddBook and GetAllBook method

---
+++ BookStore_Backend/Repository/BookRepo/BookImp.cs
@@ -0,0 +1,32 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+
+namespace Repository.BookRepo
+{
+    public class BookImp : IBook
+    {
+        private readonly BookStoreDBContext bookStoreDB;
+
+        public BookImp(BookStoreDBContext bookStoreDB)
+        {
+            this.bookStoreDB = bookStoreDB;
+        }
+        public Task<int> AddBook(Book book)
+        {
+            bookStoreDB.Book.Add(book);
+            var result = bookStoreDB.SaveChangesAsync();
+            return result;
+        }
+
+        public IEnumerable<Book> GetAllBook()
+        {
+            return bookStoreDB.Book;
+        }
+       
+    }
+}
---
+++ BookStore_Backend/Repository/Migrations/20200506094312_initial.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Repository.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Address",
+                columns: table => new
+                {
+                    BookID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FullName = table.Column<string>(nullable: false),
+                    PhoneNumber = table.Column<int>(nullable: false),
+                    Email = table.Column<string>(nullable: false),
+                    Address = table.Column<string>(nullable: false),
+                    Pincode = table.Column<int>(nullable: false),
+                    Citytown = table.Column<string>(nullable: false),
+                    Landmark = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Address", x => x.BookID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Book",
+                columns: table => new
+                {
+                    BookID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    BookTitle = table.Column<string>(nullable: false),
+                    AutherName = table.Column<string>(nullable: false),
+                    BookImage = table.Column<string>(nullable: false),
+                    BookPrice = table.Column<double>(nullable: false),
+                    NumberOfBooks = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Book", x => x.BookID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Address");
+
+            migrationBuilder.DropTable(
+                name: "Book");
+        }
+    }
+}
