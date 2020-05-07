651b15d3602b201c0a9c47e651bdb61d8b5728b9
sushant1696 <58969522+sushant1696@users.noreply.github.com>
2020-05-07 12:30:01 +0530

    [Sushant] Refactor. Added Api to the BookController.cs

---
+++ BookStore_Backend/BookStore_Api/Controllers/BookController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Manager.BookManager;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+
+namespace BookStore_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly IManager manager;
+
+        public BookController(IManager manager)
+        {
+            this.manager = manager;
+        }
+        [Route("AddBook")]
+        [HttpPost]
+        public async Task<IActionResult> AddBook(Book book)
+        {
+            var result = await this.manager.AddBook(book);
+            if (result == 1)
+            {
+                return this.Ok(book);
+            }
+            else
+            {
+                return this.BadRequest();
+            }
+        }
+        [Route("GetAllBook")]
+        [HttpGet]
+        public IEnumerable<Book> GetAllBook()
+        {
+            return this.manager.GetAllBook();
+        }
+    }
+}
