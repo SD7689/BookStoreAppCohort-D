150ceca1f5de489b39e3fc6c120a127269319bab
Anoop kumar <kanoop6401@gmail.com>
2020-05-08 19:06:50 +0530

    [Anoop] Refactor. Nnit

--- BookStore_Backend/NUnitTestBookStoreProject/AddressUnitTesing.cs
+++ BookStore_Backend/NUnitTestBookStoreProject/AddressUnitTesing.cs
@@ -1,7 +1,12 @@
-﻿using NUnit.Framework;
+﻿using BookStore_Api.Controllers;
+using Manager.CustomerManager;
+using Model;
+using Moq;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace NUnitTestBookStoreProject
 {
