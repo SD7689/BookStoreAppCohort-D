9fd6c7a69219467d073c4814b78847e642370d4a
Anoop kumar <kanoop6401@gmail.com>
2020-05-21 16:47:04 +0530

    [Anoop] Add. passwd Decode method in RL Login API

--- BookStore_Backend/BookStore_Backend/Repository/LoginRepo/LoginRepoRL.cs
+++ BookStore_Backend/BookStore_Backend/Repository/LoginRepo/LoginRepoRL.cs
@@ -33,13 +33,13 @@ namespace BookStoreRepositoryLayer.LoginRepo
         /// <returns></returns>
         public bool LoginRL(User user)
         {
-
-            var result = context.Users.Where(id => id.Email == user.Email && id.Password == user.Password).FirstOrDefault();
-            if (result == null)
+            var result = context.Users.Where(id => id.Email == user.Email).FirstOrDefault();
+            var passwd = base64Decode(result.Password);
+            if (user.Password==passwd)
             {
-                return false;
+                return true;
             }
-            return true;
+            return false;
         }
         public static string base64Encode(string password) // Encode    
         {
@@ -53,6 +53,24 @@ namespace BookStoreRepositoryLayer.LoginRepo
             catch (Exception ex)
             {
                 throw new Exception("Error in base64Encode" + ex.Message);
+            }
+        }
+        public static string base64Decode(string sData) //Decode    
+        {
+            try
+            {
+                var encoder = new System.Text.UTF8Encoding();
+                System.Text.Decoder utf8Decode = encoder.GetDecoder();
+                byte[] todecodeByte = Convert.FromBase64String(sData);
+                int charCount = utf8Decode.GetCharCount(todecodeByte, 0, todecodeByte.Length);
+                char[] decodedChar = new char[charCount];
+                utf8Decode.GetChars(todecodeByte, 0, todecodeByte.Length, decodedChar, 0);
+                string result = new String(decodedChar);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error in base64Decode" + ex.Message);
             }
         }
     }
